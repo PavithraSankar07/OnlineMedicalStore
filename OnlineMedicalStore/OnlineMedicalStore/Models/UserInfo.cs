0b143782209a10d0be84e1c8c6f4549df5b2f1b0
PavithraSankar07 <pavithra.sankar@syncfusion.com>
2025-07-11 16:22:44 +0530

    Committing the changes of add dialog box for confirmation message, Adding the home page, Add the validation for signin,signup and medicine details form.

--- OnlineMedicalStore/OnlineMedicalStore/Models/UserInfo.cs
+++ OnlineMedicalStore/OnlineMedicalStore/Models/UserInfo.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +8,17 @@ namespace OnlineMedicalStore.Models
 {
     public class UserInfo
     {
-        //  private static int s_serialnumber = 1;
-     
-        // private static int s_userID = 1;
+        
         public int UserID { get; set; }
+        [Required (ErrorMessage ="Enter the valid name")]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string MobileNumber { get; set; }
+        [Required]
         public int WalletBalance { get; set; }
         public string Role { get; set; }
 
